a9d80f5d12720e71df1b13adc7b9491fd7215fc4
caleb-freitas <calebfreitas@tutanota.com>
2022-09-30 15:45:28 -0300

    chore: set up fonts and colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,12 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <h1>Home page</h1>
+    <>
+      <h1 className="font-roboto-regular">Home page</h1>
+      <p>test roboto regular</p>
+      <h3>teste roboto light</h3>
+      <p className="f"></p>
+    </>
   )
 };
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      "helvetica": ["Helvetica", ...defaultTheme.fontFamily.sans],
+      "roboto-light": ["'Roboto Light'", ...defaultTheme.fontFamily.sans],
+      "roboto-regular": ["'Roboto Regular'", ...defaultTheme.fontFamily.sans],
+    },
+    colors: {
+      "transparent": "transparent",
+      "current": "currentColor",
+
+      "mid-blue": "#29abe2",
+      "dark-blue": "#012d51",
+      "light-gray": "#808080"
+    }
   },
   plugins: [],
 };
